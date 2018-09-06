fa679b700e42cbb4ad48e6727a4aca926cb3a8ec
Richard Cooper <richard.cooper@infuse.it>
2018-09-06 16:10:56 +0100

    Added user.js file to Models directory and added mongodb

---
+++ models/user.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+var UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  favoriteBook: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+//hash password before saving to databas
+UserSchema.pre("save", function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    } else {
+      user.password = hash;
+      next();
+    }
+  });
+});
+
+var User = mongoose.model("User", UserSchema);
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../models/user");
 
 // GET /
 router.get("/", function(req, res, next) {
@@ -21,7 +22,41 @@ router.get("/register", function(req, res, next) {
 });
 
 router.post("/register", function(req, res, next) {
-  return res.send("Registered!");
+  if (
+    req.body.name &&
+    req.body.email &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword
+  ) {
+    //confirm two passwords match
+    if (req.body.password !== req.body.confirmPassword) {
+      const err = new Error("Passwords do not match");
+      err.status = 400;
+      return next(err);
+    }
+
+    //create object with form input
+    var userData = {
+      email: req.body.email,
+      name: req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password: req.body.password
+    };
+
+    //user schemas create method to insert doc into mogo
+    User.create(userData, function(error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.redirect("/profile");
+      }
+    });
+  } else {
+    const err = new Error("All fields are mandatory.");
+    err.status = 400;
+    return next(err);
+  }
 });
 
 module.exports = router;
